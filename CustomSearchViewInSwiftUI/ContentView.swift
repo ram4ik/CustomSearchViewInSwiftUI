168ce2e0403d21858a9172fdbfe8c5779f8f38d1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-02 22:01:27 +0200

    init

--- CustomSearchViewInSwiftUI/ContentView.swift
+++ CustomSearchViewInSwiftUI/ContentView.swift
@@ -9,13 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var txt = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.gray.edgesIgnoringSafeArea(.all)
+            
+            // main view goes here...
+            SearchView(txt: $txt, data: data)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SearchView: View {
+    @Binding var txt: String
+    var data: [String]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ZStack {
+                HStack {
+                    TextField("Search", text: $txt)
+                        .padding(.trailing, 75)
+                }.padding()
+                .background(Color.white)
+                
+                if self.txt != "" {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            self.txt = ""
+                        }) {
+                            Text("Cancel")
+                        }.foregroundColor(.black)
+                    }.padding()
+                }
+            }
+            if self.txt != "" {
+                List(self.data.filter{$0.lowercased().contains(self.txt.lowercased())}, id: \.self) { i in
+                    Text(i)
+                }.frame(height: 500)
+            }
+            Spacer()
+        }.padding()
     }
 }
